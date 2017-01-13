7bf4322c5a4380ab05acb53acd60e4f2886d7f9b
Adam Goldman <adamgoldman7@gmail.com>
2017-01-14 01:50:00 +0200

    fix images path

--- client/constants/constants.js
+++ client/constants/constants.js
@@ -1,3 +1,4 @@
-const isProduction = process.env.NODE_ENV === 'production'
-export const EMAIL = isProduction ? 'info@swazisundowners.com' : 'adamgoldman7@gmail.com'
-export const GOOGLE_MAPS_API_KEY = isProduction ? 'AIzaSyArFGNUV9DhdfltcC1bNfLcHbUa14QwjlE' : 'AIzaSyCs0g0kWRALBmnoLkk04JF5RPY0zsSp7nk'
+export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+export const EMAIL = IS_PRODUCTION ? 'info@swazisundowners.com' : 'adamgoldman7@gmail.com'
+export const GOOGLE_MAPS_API_KEY = IS_PRODUCTION ? 'AIzaSyArFGNUV9DhdfltcC1bNfLcHbUa14QwjlE' : 'AIzaSyCs0g0kWRALBmnoLkk04JF5RPY0zsSp7nk'
+export const BASE_IMAGE_PATH = `${window.location.protocol}//${window.location.host}/images`
